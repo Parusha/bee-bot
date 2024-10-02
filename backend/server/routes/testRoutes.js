50f24e3c9989f5395853b8203e19229d010e829a
parushanaido <ParushaN@betsoftware.com>
2024-10-02 15:00:33 +0200

    cleaned out files

--- backend/server/routes/testRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { runTest } = require('../controllers/testController');
-
-const router = express.Router();
-
-// Define POST route to run tests
-router.post('/run-test', runTest);
-
-module.exports = router;
--- backend/server/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const server = require('./services/socketService');
-
-const port = 3001;
-
-// Start the server
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
