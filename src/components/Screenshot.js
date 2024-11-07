3621f1044b67a85f19520d3ba34a009008d397f0
parushanaido <ParushaN@betsoftware.com>
2024-11-07 14:06:06 +0200

    fix environment url

--- src/components/Screenshot.js
+++ src/components/Screenshot.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
 const Screenshot = ({ screenshotUrl }) => {
+  // Get the base URL from the environment variable
+  const serverUrl = process.env.REACT_APP_SOCKET_SERVER_URL;
+
   return (
     <div className="screenshot-section">
       <h4>Screenshot</h4>
       {screenshotUrl ? (
         <div className="screenshot-container">
-          <img src={`/images/${screenshotUrl}.png`} alt="Test Screenshot" className="resized-image" /> {/* Updated class name */}
+          <img 
+            src={`${serverUrl}/images/${screenshotUrl}.png`} 
+            alt="Test Screenshot" 
+            className="resized-image" 
+          />
           <p>
-            <a href={`/images/${screenshotUrl}.png`} target="_blank" rel="noopener noreferrer">
+            <a 
+              href={`${serverUrl}/images/${screenshotUrl}.png`} 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
               View Screenshot
             </a>
           </p>
